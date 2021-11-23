144f5f5def15a3c75b47b4b1d6509047f19705f1
jyhh1992 <jyhh1992@gmail.com>
2021-11-23 15:54:11 +0900

    moveset2

--- move_plan.py
+++ move_plan.py
@@ -6,9 +6,15 @@ tello.connect()
 
 tello.takeoff()
 
+
+#  tello move
 tello.move_up(70)
 tello.move_forward(50)
-tello.move_back(50)
+# tello.rotate_clockwise(360)
+# tello.move_back(50)
+
+tello.rotate_clockwise(180)
+tello.move_forward(50)
 
 
 
